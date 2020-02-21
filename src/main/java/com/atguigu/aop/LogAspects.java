ec183f9f8c2a58bb8a9d67d98802ba000270c2cd
Li Lei <13186102535@163.com>
2020-02-21 17:12:28 +0800

    Spring注解实现环绕通知

--- src/main/java/com/atguigu/aop/LogAspects.java
+++ src/main/java/com/atguigu/aop/LogAspects.java
@@ -1,6 +1,7 @@
 package com.atguigu.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 import java.util.Arrays;
@@ -18,26 +19,27 @@ public class LogAspects {
      * 抽取公共切入点表达式
      * 1、本类引用: logStart()、logEnd()、logException
      * 2、其他的切面引入: logReturn()
-     *
-     *  public int com.atguigu.aop.MathCalculator.*(..)
-     *      public: 访问修饰符,("里边写切入表达式")
-     *      int: 返回值类型
-     *      *: 表示MathCalculator类中的所有方法
-     *      (..): 任意参数
+     * <p>
+     * public int com.atguigu.aop.MathCalculator.*(..)
+     * public: 访问修饰符,("里边写切入表达式")
+     * int: 返回值类型
+     * *: 表示MathCalculator类中的所有方法
+     * (..): 任意参数
      */
     @Pointcut("execution(public int com.atguigu.aop.MathCalculator.*(..))")
-    public void pointCut(){
+    public void pointCut() {
 
     }
 
     /**
      * 这个@Before是org.aspectj.lang.annotation.Before的
-     * @Before: 切入时机,在目标方法之前切入
      *
+     * @Before: 切入时机, 在目标方法之前切入
+     * <p>
      * 注意: JoinPoint必须出现在参数的第一位,否则Spring是无法识别的
      */
     @Before("pointCut()")
-    public void logStart(JoinPoint joinPoint){
+    public void logStart(JoinPoint joinPoint) {
         //获取参数列表
         Object[] argsList = joinPoint.getArgs();
         //joinPoint.getSignature().getName(): 获得方法名
@@ -45,17 +47,43 @@ public class LogAspects {
     }
 
     @After("pointCut()")
-    public void logEnd(JoinPoint joinPoint){
+    public void logEnd(JoinPoint joinPoint) {
         System.out.println(joinPoint.getSignature().getName() + " 方法结束...@After");
     }
 
     @AfterReturning(value = "com.atguigu.aop.LogAspects.pointCut()", returning = "result")
-    public void logReturn(JoinPoint joinPoint,Object result){
+    public void logReturn(JoinPoint joinPoint, Object result) {
         System.out.println(joinPoint.getSignature().getName() + " 方法正常返回...@AfterReturning: 计算结果:{ " + result + " }");
     }
 
     @AfterThrowing(value = "pointCut()", throwing = "exception")
-    public void logException(JoinPoint joinPoint,Exception exception){
+    public void logException(JoinPoint joinPoint, Exception exception) {
         System.out.println(joinPoint.getSignature().getName() + " 方法异常...异常信息:{" + exception + "}");
     }
+
+    /**
+     * 环绕通知,需要携带ProceedingJoinPoint类型的参数
+     * 环绕通知类似于动态代理的全过程: ProceedingJoinPoint类型的参数可以决定是否执行目标方法
+     * 且环绕通知必须有返回值,返回值即为目标方法的返回值
+     */
+    @Around(value = "pointCut()")
+    public Object logAround(ProceedingJoinPoint proceedingJoinPoint) {
+        try {
+            // 前置通知
+            System.out.println("The method " + proceedingJoinPoint.getSignature().getName() +
+                    " begins with " + Arrays.asList(proceedingJoinPoint.getArgs()));
+            // 后置通知
+            // proceed(): Proceed with the next advice or target method invocation
+            System.out.println("The method " + proceedingJoinPoint.getSignature().getName() +
+                    " ends with " + proceedingJoinPoint.proceed());
+        } catch (Throwable throwable) {
+            // 异常通知
+            System.out.println("The method " + proceedingJoinPoint.getSignature().getName() +
+                    " occurs exception: " + throwable);
+        }
+        // 后置通知
+        System.out.println("The method " + proceedingJoinPoint.getSignature().getName() + " ends ");
+        return 520;
+    }
 }
+
